4ecd15633b0e58475d26cdf51cadb790f4fd4b27
master-ladvil <srihariharanb21@gmail.com>
2020-08-06 17:51:31 +0530

    heroku commit

---
+++ routes/classifiedRoutes.js
@@ -0,0 +1,39 @@
+const router = require("express").Router()
+const linkmodel = require('../schema/mongoschema')
+
+
+router.get('/:linkdomain', async (req,res) =>{
+    const searchlink = req.params.linkdomain
+    try
+    {
+    const speclink = await linkmodel.findOne({"link":searchlink})
+    if (speclink === null){
+        res.send("1")
+    }
+    else{
+    res.send("0")
+    }
+    }catch(err){
+        res.json({
+            message: err
+        })
+    }
+})
+
+router.post('/', async (req,res) => {
+    const nlink = new linkmodel({
+        link:req.body.link
+    })
+    try{
+    const addlink = await nlink.save()
+    res.json(addlink)
+    }
+    catch(err){
+        res.json({
+            message:err
+        })
+    }
+})
+
+
+module.exports = router
